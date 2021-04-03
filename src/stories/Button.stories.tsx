8c4d491d325ca6a5ed2928b13eaa6d5037213710
JinleeJeong <pjjr0118@gmail.com>
2021-04-03 14:16:40 +0900

    refactor: fix the file structure & tsconfig

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import Button from '../components/Button';
+import Button from '../components/atoms/Button';
 
 storiesOf('Button', module).add('with text', () => {
   return <Button text='Click Me' />;
