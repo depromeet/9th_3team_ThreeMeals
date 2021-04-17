8b71902fec895f0e3b667a1666f94342e405a173
youngsoohan <amazingmobdev@gmail.com>
2021-04-17 17:14:52 +0900

    feat: add BundleAnalyzer and fix viewport meta tag location

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
+import React from 'react'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'https://countries-274616.ew.r.appspot.com',
   cache: new InMemoryCache(),
-});
+})
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   React.useEffect(() => {
-    window.Kakao.init(process.env.NEXT_PUBLIC_JAVASCRIPT_KEY);
-    window.Kakao.isInitialized();
-  }, []);
+    window.Kakao.init(process.env.NEXT_PUBLIC_JAVASCRIPT_KEY)
+    window.Kakao.isInitialized()
+  }, [])
 
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </ApolloProvider>
-  );
-};
+  )
+}
 
-export default MyApp;
+export default MyApp
