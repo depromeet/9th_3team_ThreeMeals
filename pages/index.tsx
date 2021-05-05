1ca9b5e43723ab84b7a0b12f77be2916c40b06f2
JinleeJeong <pjjr0118@gmail.com>
2021-05-05 12:05:39 +0900

    refactor: remove unnecessary code & fit the GlobalStyle

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import { useQuery } from '@apollo/client'
 import QUERY_COUNTRIES from './queryCountries.graphql'
-import styled from 'styled-components'
 import { IMAGES } from '../src/constants/images'
 import Link from 'next/link'
 
@@ -34,7 +33,6 @@ const Home: React.FC<Props> = (prop) => {
         <title>Three_Meals</title>
       </Head>
       <h1>Three_Meals</h1>
-      <StyledButton>styled Btn</StyledButton>
       <img src={IMAGES.background} width={20} />
       <button
         onClick={() => {
@@ -71,15 +69,5 @@ const Home: React.FC<Props> = (prop) => {
     </div>
   )
 }
-
-const StyledButton = styled.button`
-  padding: 0.375rem 0.75rem;
-  border-radius: 0.25rem;
-  font-size: 1rem;
-  line-height: 1.5;
-  border: 1px solid lightgray;
-  color: gray;
-  background: white;
-`
 
 export default Home
--- src/utils/GlobalStyle.ts
+++ src/utils/GlobalStyle.ts
@@ -4,9 +4,15 @@ import reset from 'styled-reset'
 export const GlobalStyle = createGlobalStyle`
   ${reset};
 
-  html,
+  html {height: 100%}
   body {
     background: #191919;
+    height: 100%;
+  }
+  body > div:first-child,
+  div#__next,
+  div#__next > div {
+    height: 100%;
   }
   #root {
     height: 100%;
@@ -28,4 +34,5 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     cursor: pointer;
   }
+  
 `
