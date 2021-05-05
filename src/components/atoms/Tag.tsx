9a95f0ef3ddf0c671b99997884f865b7155337d6
JinleeJeong <pjjr0118@gmail.com>
2021-05-05 12:08:24 +0900

    refactor: add the props to remove tag

--- src/components/atoms/Tag.tsx
+++ src/components/atoms/Tag.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+import { IMAGES } from '../../constants/images'
 import NextButton from './NextButton'
 
 interface Props {
+  id?: string
   href: string
   text: string
   icon?: string
+  onClickClose?: (id: string) => void
 }
 
-const StyledTag = styled(NextButton)`
+const Container = styled.div`
   font-family: SF Pro Display;
   font-size: 15px;
   line-height: 22px;
@@ -17,26 +20,45 @@ const StyledTag = styled(NextButton)`
   color: white;
   background-color: #242424;
   border-radius: 11px;
-  padding: 0 8px;
   display: flex;
   justify-content: center;
   align-items: center;
   height: 22px;
+  padding: 0 8px;
+`
+const StyledTag = styled(NextButton)`
+  color: white;
 `
 const Icon = styled.img`
   width: 16px;
   height: 16px;
   margin-right: 4px;
+  position: relative;
+  top: 2.5px;
+`
+
+const CloseIcon = styled.img`
+  width: 14px;
+  height: 14px;
+  margin-left: 8px;
 `
 const Text = styled.span`
   margin-top: 2px;
 `
 const Tag: React.FunctionComponent<Props> = (props) => {
   return (
-    <StyledTag href={props.href}>
-      <Icon src={props.icon} />
-      <Text>{props.text}</Text>
-    </StyledTag>
+    <Container>
+      <StyledTag href={props.href}>
+        <Icon src={props.icon} />
+        <Text>{props.text}</Text>
+      </StyledTag>
+      <CloseIcon
+        src={IMAGES.icon_24_close_green_wh}
+        onClick={() => {
+          props.onClickClose && props.id && props.onClickClose(props.id)
+        }}
+      />
+    </Container>
   )
 }
 
