8227b3c461771b0797da943efec8797963db1fae
JinleeJeong <pjjr0118@gmail.com>
2021-05-09 19:42:03 +0900

    refactor: remove the comment

--- src/components/templates/ProfileEditTemplate.tsx
+++ src/components/templates/ProfileEditTemplate.tsx
@@ -34,7 +34,6 @@ const ProfileEditTemplate: React.FC<Props> = (props: Props) => {
           ref={textareaRef}
           value={currentValue}
           onChange={(e) => {
-            console.log('e:', e.target.value)
             setCurrentValue(e.target.value)
           }}
           placeholder={'소개글을 작성해주세요.'}
