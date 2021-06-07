6ac96883117eef0f99eee7d2cdcb0a76dc582463
JinleeJeong <pjjr0118@gmail.com>
2021-06-07 20:51:34 +0900

    feat: connect api about getPost on ContentTemplate

---
+++ src/lib/queries/getPostQueries.ts
@@ -0,0 +1,83 @@
+import { gql } from '@apollo/client'
+
+export interface getPostParams {
+  first: number
+  accountId?: string
+}
+export interface postCount {
+  count: number
+  postType: string
+}
+
+interface getPostEdges {
+  node: {
+    id: string
+    content: string
+    postType: string
+    secretType: string
+    fromAccount: {
+      id: string
+    }
+    usedEmoticons: {
+      id: string
+      positionX: number
+      positionY: number
+      emoticon: {
+        id: string
+        fileUrl: string
+      }
+    }[]
+  }
+  cursor: string
+}
+
+export interface getPost {
+  getMyNewPostCount: {
+    postCount: postCount[]
+  }
+  getPosts: {
+    edges: getPostEdges[]
+    pageInfo: {
+      endCursor: string
+      hasNextPage: boolean
+    }
+  }
+}
+
+export const GET_POST = gql`
+  query {
+    getMyNewPostCount(postType: Answer) {
+      postCount {
+        count
+        postType
+      }
+    }
+    getPosts(first: 10, accountId: "5") {
+      edges {
+        node {
+          id
+          content
+          postType
+          secretType
+          fromAccount {
+            id
+          }
+          usedEmoticons {
+            id
+            positionX
+            positionY
+            emoticon {
+              id
+              fileUrl
+            }
+          }
+        }
+        cursor
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
+    }
+  }
+`
