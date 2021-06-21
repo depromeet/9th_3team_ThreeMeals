5509f69d832fb4f241e27ccdd431a47f27c85718
JinleeJeong <pjjr0118@gmail.com>
2021-06-21 17:15:18 +0900

    feat: connect to props & refactoring & fix the route

--- src/components/molecules/ProfileContent.tsx
+++ src/components/molecules/ProfileContent.tsx
@@ -15,9 +15,7 @@ const ProfileContent: React.FC<Props> = (props: Props) => {
       <NameText>{props.name}</NameText>
       <DescText>{props.desc}</DescText>
       <div style={{ display: 'flex' }}>
-        <Tag text={props.urlName} isNonClose>
-          {props.url}
-        </Tag>
+        <Tag text={props.urlName} isNonClose url={props.url} />
       </div>
     </Container>
   )
