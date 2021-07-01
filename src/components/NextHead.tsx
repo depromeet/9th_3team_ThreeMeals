d921865ef752fd64dc2d5f9dc1423fde6a6d3b53
Yeongsu Han <amazingmobdev@gmail.com>
2021-07-01 15:48:55 +0900

    feat: makeup next head (#119)

---
+++ src/components/NextHead.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react'
+import Head from 'next/head'
+
+interface Props {
+  title?: string
+  description?: string
+  image?: string
+}
+
+const NextHead: FC<Props> = ({ title, description, image }) => {
+  const ogTitle = title || 'hush'
+  const ogDesc = description || 'MZ세대를 위한 소통플랫폼'
+  const ogImage = image || '/ogimage.png'
+
+  return (
+    <Head>
+      <title>Hush</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      />
+      <meta property="og:title" content={ogTitle} />
+      <meta name="description" content={ogDesc} />
+      <meta property="og:description" content={ogDesc} />
+      <meta property="og:image" content={ogImage} />
+    </Head>
+  )
+}
+
+export default NextHead
