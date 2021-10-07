baea1b3d716e20eb0b521de8d5d848605cf1a044
YH <slash9494@naver.com>
2021-10-08 07:04:08 +0900

    feat: add meta infomations

--- src/components/NextHead.tsx
+++ src/components/NextHead.tsx
@@ -8,9 +8,16 @@ interface Props {
 }
 
 const NextHead: FC<Props> = ({ title, description, image }) => {
-  const ogTitle = title || 'hush'
-  const ogDesc = description || 'MZ세대를 위한 소통플랫폼'
+  const ogTitle = title || 'hush-it'
+  const ogSiteName = 'hush-it'
   const ogImage = image || '/ogimage.png'
+  const ogType = 'website'
+
+  const desc =
+    description ||
+    'MZ세대를 위한 소통플랫폼, 익명질문, OX질문, 답변받기, 스티커 카드, 스티커 캐릭터'
+  const keywords =
+    'hush,hush-it,허쉬,허쉬잇,MZ세대,질문,답변,익명,익명질문,스티커,OX질문,'
 
   return (
     <Head>
@@ -19,10 +26,16 @@ const NextHead: FC<Props> = ({ title, description, image }) => {
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
       />
+
+      <meta name="description" content={desc} />
+      <meta name="keywords" content={keywords} />
+
       <meta property="og:title" content={ogTitle} />
-      <meta name="description" content={ogDesc} />
-      <meta property="og:description" content={ogDesc} />
+      <meta property="og:site_name" content={ogSiteName} />
+      <meta property="og:description" content={desc} />
       <meta property="og:image" content={ogImage} />
+      <meta property="og:type" content={ogType} />
+      <meta property="og:url" content="https://hush-it.com/" />
 
       <meta
         name="google-site-verification"
@@ -37,6 +50,15 @@ const NextHead: FC<Props> = ({ title, description, image }) => {
       <meta name="NaverBot" content="index,follow" />
       <meta name="Yeti" content="All" />
       <meta name="Yeti" content="index,follow" />
+
+      <link rel="apple-touch-icon" href="/ogimage.png" />
+      <link rel="canonical" href="https://hush-it.com/" />
+      <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        sizes="16x16 32x32 64x64"
+        href="/favicon.ico"
+      />
     </Head>
   )
 }
