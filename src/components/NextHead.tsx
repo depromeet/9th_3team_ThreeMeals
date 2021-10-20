3f454383ad647d74b32d2ec283877d6f3d3d1b54
YH <slash9494@naver.com>
2021-10-21 01:03:19 +0900

    chore: fix head - title

--- src/components/NextHead.tsx
+++ src/components/NextHead.tsx
@@ -21,7 +21,7 @@ const NextHead: FC<Props> = ({ title, description, image }) => {
 
   return (
     <Head>
-      <title>Hush</title>
+      <title>Hush-it</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
